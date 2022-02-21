4fe31eaf1354ddb648675148343563f8cbc45af7
Christine Rowland <fordo29@gmail.com>
2022-02-21 15:35:10 -0700

    Start building App component

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import '../styles/App.css';
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <h1>book club</h1>
+    )
+  }
+}
+
+export default App;
