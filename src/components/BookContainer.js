c5ef7a37f95163ec1cbbd02baea3edc43db42de8
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-21 16:42:34 -0700

    Add details to book container

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -2,11 +2,22 @@ import React from "react";
 import '../styles/BookContainer.css';
 import BookCard from "./BookCard";
 
-const BookContainer = (props) => {
-
-    return(
-        <h2>Hi Ethan's books container</h2>
+const BookContainer = ({ allBooks }) => {   
+  const bookCards = allBooks.maps((book) => {
+    return (
+      <BookCard 
+        key={book.isbn}
+        title={book.title}
+        bookImage={book.bookImage}
+        author={book.author}
+      />
     )
+  })
+  return (
+    <section className="book-container">
+      {bookCards}
+    </section>
+  )
 }
 
 export default BookContainer;
