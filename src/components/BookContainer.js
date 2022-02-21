e8ba576ed7976b3a1b7f20bc9913b2ccba651ffa
Christine Rowland <fordo29@gmail.com>
2022-02-21 15:41:58 -0700

    Create BookContainer component

---
+++ src/components/BookContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles/BookContainer.css';
+
+const BookContainer = (props) => {
+
+    return(
+        <h2>Hi Ethan's books container</h2>
+    )
+}
+
+export default BookContainer;
