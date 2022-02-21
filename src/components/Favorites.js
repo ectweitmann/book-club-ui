b6f468c2fdf3590d31bfe75285e9a3335c661fdc
Christine Rowland <fordo29@gmail.com>
2022-02-21 16:02:53 -0700

    Create Favorite component

---
+++ src/components/Favorites.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import '../styles/Favorites.css';
+
+class Favorites extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <h2>Favs here</h2>
+        )
+    }
+}
+
+export default Favorites;
