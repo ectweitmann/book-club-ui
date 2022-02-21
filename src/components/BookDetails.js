fb988431f695bf6c9e59f68321cc75e7ebf55b74
Christine Rowland <fordo29@gmail.com>
2022-02-21 15:56:11 -0700

    Add Book Details component

---
+++ src/components/BookDetails.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import '../styles/BookDetails';
+
+class BookDetails extends Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <h3>details for some stuff</h3>
+        )
+    }
+}
+
+export default BookDetails;
