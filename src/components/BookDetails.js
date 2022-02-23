dbc3f47213dff1e40a096c84fbba5b015c286185
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-22 17:23:33 -0700

    Add details to render method

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -2,16 +2,34 @@ import React, { Component } from "react";
 import '../styles/BookDetails.css';
 
 class BookDetails extends Component {
-    constructor() {
-        super();
-        this.state = {}
+  constructor() {
+    super();
+    this.state = {
+      selectedBook: {},
+      error: ''
     }
+  }
 
-    render() {
-        return (
-            <h3>details for some stuff</h3>
-        )
-    }
+  componentDidMount = () => {
+
+  }
+
+  render() {
+    return (
+        <section className="book-details">
+          <img src={this.state.selectedBook.bookImage}/>
+          <div className="book-details-text-container">
+            <h2>{this.state.selectedBook.title}</h2>
+            <p>{this.state.selectedBook.author}</p>
+            <p>{this.state.selectedBook.description}</p>
+            <div className="button-container">
+              <button>Add to Favorites</button>
+              <a href="{this.state.selectedBook.amazonLink}"></a>
+            </div>
+          </div>
+        </section>
+      )
+  }
 }
 
 export default BookDetails;
