85537cd945a3a39c769881c0a9eedcc22f45667e
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-25 09:35:02 -0800

    Add minor styling changes to multiple components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Nav.css';
 
 const Nav = () => {
   return (
     <nav className="navbar">
       <h1 className="page-title">Book Club</h1>
+      <Link className="favorites-link" to="/favorites">Your Favorites »</Link>
     </nav>
   )
 }
