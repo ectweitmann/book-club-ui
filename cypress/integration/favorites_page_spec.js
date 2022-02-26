e17b3e9276f013c77df447e6a7652707ad50a8b0
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-26 15:04:44 -0700

    Add testing to favorites page spec

--- cypress/integration/favorites_page_spec.js
+++ cypress/integration/favorites_page_spec.js
@@ -15,4 +15,24 @@ describe('Book Club favorites page user flow', () => {
       .get('.book-cover').last().should('have.attr', 'src', 'https://storage.googleapis.com/du-prd/books/images/9781250278210.jpg')
       .get('.learn-more-btn').contains('Learn More')
     })
+
+  it('should display a message to user when there are no books favorited', () => {
+    cy.intercept('https://book-club-api-2110.herokuapp.com/api/v1/favorites', { favorites: [] })
+    cy.visit('http://localhost:3000/favorites')
+      .get(".logo").should('have.attr', 'alt', 'Book club logo')
+      .get('.landing-page-link').contains('Return Home »')
+      .get('.book-card').should('have.length', 0)
+      .get('.empty-favorites-msg').contains('You haven\'t added any favorites yet -- get to it!')
+  })
+
+  it('should allow a user to return to landing page', () => {
+    cy.intercept('https://book-club-api-2110.herokuapp.com/api/v1/favorites', { favorites: 'favoritedBooks.json' })
+    cy.visit('http://localhost:3000/favorites')
+      .get(".logo").should('have.attr', 'alt', 'Book club logo').click()
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/')
+    })
+      .get('.favorites-link').contains('Your Favorites »')
+  })
+
 })
