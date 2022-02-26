95188235ee04effb34256e6d38b33b4cf74a7b47
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-26 12:31:37 -0800

    Create initial component and styling files for Error404 component

---
+++ src/components/Error404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Error404.css';
+
+const Error404 = () => {
+  return (
+    <div className="error-404-page">
+      <h2 className="error">PAGE NOT FOUND.</h2>
+      <p className="message">We are so sorry, but this page does not exist</p>
+    </div>
+  );
+}
+
+export default Error404;
