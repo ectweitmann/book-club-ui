19fe8ce068a5c2769894a275bb8494e411286103
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-26 11:29:36 -0800

    Add conditional rendering for navLink

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom';
 import logo from '../Book_Club_Logo.png';
 import '../styles/Nav.css';
 
-const Nav = () => {
+const Nav = ({ location }) => {
+  let navLink;
+  if (location === 'favorites') {
+    navLink = <Link className="landing-page-link" to="/">Return Home »</Link>
+  } else {
+    navLink = <Link className="favorites-link" to="/favorites">Your Favorites »</Link>
+  }
   return (
     <nav className="navbar">
-      <Link className="landing-page-link" to="/">
+      <Link className="logo-link" to="/">
         <img className="logo" src={logo} alt="Book club logo"/>
       </Link>
-      <Link className="favorites-link" to="/favorites">Your Favorites »</Link>
+      {navLink}
     </nav>
   )
 }
