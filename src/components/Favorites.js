002f077f96b45c9d25b5dd4dd3b78170b681e491
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-26 10:28:02 -0800

    Add BookContainer component and lifecycle method

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,20 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { apiCalls } from '../apiCalls.js';
+import { cleanBookData } from '../utils';
+import BookContainer from './BookContainer';
 import '../styles/Favorites.css';
 
 class Favorites extends Component {
-    constructor() {
-        super();
-        this.state = {
-
-        }
+  constructor() {
+    super();
+    this.state = {
+      favorites: []
     }
-    
+  }
 
-    render() {
-        return (
-            <h2>Favs here</h2>
-        )
-    }
+  componentDidMount = () => {
+    apiCalls.getAllFavorites()
+      .then(booksData => {
+        const cleanedBooksData = booksData.map(book => cleanBookData(book));
+        this.setState({ favorites: cleanedBooksData})
+      })
+      .catch(error => this.setState({ error: error.message}))
+  }
+
+  render() {
+    return (
+      <BookContainer allBooks={this.state.favorites}/>
+    )
+  }
 }
 
 export default Favorites;
