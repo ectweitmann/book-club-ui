62903db045d9690a38da08c9823de72c5d7184d8
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-26 12:29:53 -0800

    Add alt text attribute and update path for 'learn more' button

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const BookCard = ({title, author, bookImage, isbn }) => {
     return (
       <section className="book-card">
-        <img className="book-cover" src={bookImage}/>
+        <img className="book-cover" src={bookImage} alt={`${title} cover`}/>
         <div className="book-side-panel">
           <div className="book-side-panel-banner">
             <h2 className="book-title">{title}</h2>
             <p className="book-author">{author}</p>
           </div>
-          <Link to={`/${isbn}`} className="learn-more-btn">Learn More</Link>
+          <Link to={`/${isbn}/selectedBook`} className="learn-more-btn">Learn More</Link>
         </div>
       </section>
     )
