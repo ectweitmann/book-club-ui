a1a60ea54c6834a11b2c02ec7a60bb0ab40a2294
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-26 10:56:59 -0800

    Add Link and img tag for logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../Book_Club_Logo.png';
 import '../styles/Nav.css';
 
 const Nav = () => {
   return (
     <nav className="navbar">
-      <h1 className="page-title">Book Club</h1>
+      <Link className="landing-page-link" to="/">
+        <img className="logo" src={logo} alt="Book club logo"/>
+      </Link>
       <Link className="favorites-link" to="/favorites">Your Favorites »</Link>
     </nav>
   )
