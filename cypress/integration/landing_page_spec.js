3ba973fbf3e3bd860c4b9b5b80f5884185bef123
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-26 15:07:47 -0700

    Edit classnames for landing page test

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -3,12 +3,12 @@ describe('Book Club landing page user flow', () => {
   it('should display page title and all of the book cards', () => {
     cy.intercept('https://book-club-api-2110.herokuapp.com/api/v1/books', { fixture: 'landingPageBooks.json' })
     cy.visit('http://localhost:3000')
-      .get('h1').contains('Book Club')
+      .get(".logo").should('have.attr', 'alt', 'Book club logo')
       .get('.book-card').should('have.length', 5)
       .get('.book-card').contains('Abandoned in Death')
       .get('.book-card').contains('J.D. Robb')
-      .get('img').should('have.attr', 'src', 'https://storage.googleapis.com/du-prd/books/images/9781250278210.jpg')
-      .get('button').contains('Learn More')
+      .get('.book-cover').should('have.attr', 'src', 'https://storage.googleapis.com/du-prd/books/images/9781250278210.jpg')
+      .get('.learn-more-btn').contains('Learn More')
   });
 
 });
