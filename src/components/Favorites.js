e8f5e90050df3b454d5afd7c920df2e483056069
Kayla Durrett <kayla.durrett@gmail.com>
2022-02-27 12:41:00 -0700

    Add error handle logic to render to show ErrorModal when error occurs

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { apiCalls } from '../apiCalls.js';
 import { cleanBookData } from '../utils';
 import BookContainer from './BookContainer';
+import ErrorModal from './ErrorModal.js';
 import '../styles/Favorites.css';
 
 class Favorites extends Component {
   constructor() {
     super();
     this.state = {
-      favorites: []
+      favorites: [],
+      error: ''
     }
   }
 
@@ -22,10 +24,12 @@ class Favorites extends Component {
   }
 
   render() {
+    const errorModal = this.state.error ? <ErrorModal message={this.state.error}/> : null
     return (
       <>
         {!this.state.favorites.length && <h2 className="empty-favorites-msg">You haven't added any favorites yet -- get to it!</h2>}
         <BookContainer allBooks={this.state.favorites}/>
+        {errorModal}
       </>
     )
   }
